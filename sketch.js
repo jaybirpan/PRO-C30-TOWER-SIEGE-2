b5da24f322ed5f08fee0bd0e6bb772452b57624e
jaybirpan <74968727+jaybirpan@users.noreply.github.com>
2021-02-28 23:51:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -63,7 +63,7 @@ function setup() {
   ball = Bodies.circle(50,200,20);
   World.add(world,ball);
 
-  slingShot = new SlingShot(this.ball,{x:200,y:200});
+  slingshot = new Slingshot(this.ball,{x:200,y:200});
 
 }
 
